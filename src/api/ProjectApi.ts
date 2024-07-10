651c4f0495021cc6ab5e1bcc00478096b014e78b
Germán David <manchov12@gmail.com>
2024-07-10 11:18:22 -0500

    installing react query - creating proyects with useMutation

--- src/api/ProjectApi.ts
+++ src/api/ProjectApi.ts
@@ -1,11 +1,15 @@
 import api from "@/lib/axios";
 import { ProjectFormData } from "../types";
+import { isAxiosError } from "axios";
 
 export async function createProject(formData : ProjectFormData) {
     try {
         const { data } = await api.post('/projects', formData)
-        console.log(data);
+        return data
     } catch (error) {
+        if (isAxiosError(error) && error.response ) {
+            throw new Error(error.response.data.error)
+        }
         console.log(error);
     }
 }
--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,10 +1,15 @@
 import ProjectForm from "@/components/projects/ProjectForm";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+
+import { Link, useNavigate } from "react-router-dom";
 import { ProjectFormData } from "@/types/index";
 import { createProject } from "@/api/ProjectApi";
+import { toast } from "react-toastify";
+import { useMutation } from "@tanstack/react-query";
 
 export default function CreateProjectView() {
+
+    const navigate = useNavigate()
 
     const intialValues : ProjectFormData = {
         projectName: '',
@@ -14,10 +19,19 @@ export default function CreateProjectView() {
 
     const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: intialValues })
 
-    const handleForm = (data : ProjectFormData) => {
-        createProject(data)
-    }
-
+    const {mutate} = useMutation({
+        mutationFn: createProject,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+            navigate('/')
+        }
+    })
+    
+    const handleForm =  (formData : ProjectFormData) => mutate(formData)        
+    
     return (
         <>
             <div className="max-w-3xl mx-auto">
