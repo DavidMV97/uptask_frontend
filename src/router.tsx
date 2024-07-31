292a59a1cbae060fe1f13f0154cfb320b951dd01
Germán David <manchov12@gmail.com>
2024-07-31 09:59:15 -0500

    views register and login

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,9 @@ import DashboardView from "@/views/DashboardView";
 import CreateProjectView from "@/views/projects/CreateProjectView";
 import EditProjectView from "@/views/projects/EditProjectView";
 import ProjectsDetailsView from "@/views/projects/ProjectsDetailsView";
+import AuthLayout from "./layouts/AuthLayout";
+import LoginView from "./views/auth/LoginView";
+import RegisterView from "./views/auth/RegisterView";
 
 
 export default function AppRouter() {
@@ -17,6 +20,12 @@ export default function AppRouter() {
                     <Route path="/projects/:projectId" element={<ProjectsDetailsView />}  />
                     <Route path="/projects/:projectId/edit" element={<EditProjectView />}  />
                 </Route>
+
+                <Route element={<AuthLayout />} >
+                    <Route path='/auth/login' element={<LoginView />} />
+                    <Route path='/auth/register' element={<RegisterView />} />
+                </Route>
+
             </Routes>
         </BrowserRouter>
     )
